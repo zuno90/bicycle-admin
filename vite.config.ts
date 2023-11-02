4d6dadfbb32321b6f1c76e4c8087db062b6be536
zuno90 <pnlan1406@gmail.com>
2023-11-02 16:52:44 +0700

    deploy prod

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   define: process.env.NODE_ENV === "development" ? { global: "window" } : {},
+  resolve: {
+    alias: { "./runtimeConfig": "./runtimeConfig.browser" },
+  },
 });
